fd0398c27b597f50498081319bc3133890ad3c6e
pupil <pupil@ivkhk.ee>
2019-11-14 11:24:43 +0200

    14.11.2019

---
+++ Tund3_OOP/index.php
@@ -0,0 +1,10 @@
+<?php
+require 'model/Category.php'; // подключаем нужные модели
+
+include_once 'inc/Database.php'; // класс для работы с базой
+
+include_once 'controller/Controller.php'; // контроллер
+include_once 'route/routing.php'; // маршрутизатор
+
+echo $response;
+?>
